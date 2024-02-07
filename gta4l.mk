52539f2575c6b6b625d8f62ed1017c34b81b7d99
DY3413 <milosix3413@gmail.com>
2024-02-08 01:18:34 +0900

    gta4l-common: overlay: Update and clean up

--- gta4l.mk
+++ gta4l.mk
@@ -40,8 +40,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
